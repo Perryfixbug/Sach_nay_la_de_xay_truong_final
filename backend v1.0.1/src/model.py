8fbd8976c696e7ecb3d12a7aafe367d7b401187b
Perryfixbug <hieupmdkim@gmail.com>
2024-09-25 20:53:32 +0700

    api cart

--- backend v1.0.1/src/model.py
+++ backend v1.0.1/src/model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class Product(db.Model):
     id = Column(String(20), primary_key=True)
     name = Column(String(100), nullable=False)
-    img = Column(String(100), nullable=False)
+    img = Column(String(100), nullable=True)
     price = Column(Integer, nullable=False)
     author = Column(String(50), nullable=False)
     detail = Column(String(200), nullable=True)
@@ -28,8 +28,8 @@ class Product(db.Model):
 
 class PCart(db.Model):
     id = Column(String(20), ForeignKey('product.id'), primary_key=True)
-    date = Column(String(20), default=datetime.now)
-    num = Column(Integer, default=1)
+    date = Column(String(20), default=datetime.today().date())
+    quantity = Column(Integer, default=1)
     product = relationship('Product', back_populates='pcarts', lazy = True)
     def to_dict(self):
         if self.product:
@@ -43,7 +43,7 @@ class PCart(db.Model):
                 'date': self.date, 
                 'detail': product_info.detail,
                 'category': product_info.category,
-                'num': self.num
+                'quantity': self.quantity
             }
         return {}
 
