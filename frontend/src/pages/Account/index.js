60620d8088dba8272048eea43e34b092ec6a3851
Perryfixbug <hieupmdkim@gmail.com>
2024-10-19 19:38:23 +0700

    add guest account

--- frontend/src/pages/Account/index.js
+++ frontend/src/pages/Account/index.js
@@ -56,6 +56,7 @@ const Account = () => {
       localStorage.removeItem('mess');
       localStorage.removeItem('access_token');
       navigate('/validation');
+      window.location.reload();
     } catch (error) {
       console.error('Failed to log out:', error);
     }
